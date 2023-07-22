0579f9114732df6d4be4427d8b8758bd687d15be
arvindpndit <arvindpandit9808@gmail.com>
2023-07-22 10:37:35 +0530

    03 variable ts code added

---
+++ 01 hello typescript/03-variable2.ts
@@ -0,0 +1,7 @@
+let num = 7;
+num.toString();
+//num.toUpperCase -> error because toUpperCase function is available for number
+
+let nameOfCustomer = "arvind pandit";
+nameOfCustomer.toUpperCase();
+console.log(nameOfCustomer);
