d337adb53ce8cab626374ae161dd3cc271b88ab5
arvindpndit <arvindpandit9808@gmail.com>
2023-07-22 22:20:37 +0530

    added the array code

---
+++ 04 arrays/01-array.ts
@@ -0,0 +1,8 @@
+const students: string[] = ["arvind", "arvind2", "arvind3"];
+
+const marks: number[] = [89, 78, 76, 88, 98];
+
+const products: Array<string> = ["maggie", "namkeen", "rice"];
+
+products.push("fruits");
+//products.push(6); throws an error
---
+++ 04 arrays/02-arrray.ts
@@ -0,0 +1,5 @@
+const rbgConfig: Array<Array<number>> = [
+  [223, 434, 112],
+  [222, 434, 122],
+  [223, 453, 152],
+];
